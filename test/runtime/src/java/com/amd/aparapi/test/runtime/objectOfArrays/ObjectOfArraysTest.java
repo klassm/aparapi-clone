fb47d810695fa4c7452de142b3410262413dc249
Matthias Klass <matthias@klass.li>
2013-08-21 11:54:30 +0200

    inline tests

--- test/runtime/src/java/com/amd/aparapi/test/runtime/objectOfArrays/ObjectOfArraysTest.java
+++ test/runtime/src/java/com/amd/aparapi/test/runtime/objectOfArrays/ObjectOfArraysTest.java
@@ -2,6 +2,8 @@ package com.amd.aparapi.test.runtime.objectOfArrays;
 
 import com.amd.aparapi.EXECUTION_MODE;
 import com.amd.aparapi.internal.kernel.KernelRunner;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -11,11 +13,16 @@ import static org.junit.Assert.assertThat;
 
 public class ObjectOfArraysTest {
 
+   private KernelRunner kernelRunner;
+
+   @Before
+   public void before() {
+      kernelRunner = new KernelRunner();
+      kernelRunner.setExplicit(true);
+   }
+
    @Test
    public void testObjectOfArrays() {
-      KernelRunner kernelRunner = new KernelRunner();
-      kernelRunner.setExplicit(true);
-
       int[] data = new int[] { 3, 5, 7, 9, 11, 12};
       int[] dataCopy = Arrays.copyOf(data, data.length);
 
@@ -32,5 +39,39 @@ public class ObjectOfArraysTest {
       }
 
       assertThat(kernelRunner.getExecutionMode(), is(EXECUTION_MODE.GPU));
+   }
+
+   @Test
+   public void testObjectOfArraysMultipleHolder() {
+      int[] data1 = new int[] { 3, 5, 7, 9, 11, 12};
+      int[] data2 = Arrays.copyOf(data1, data1.length);
+
+      Holder holder1 = new Holder(data1);
+      holder1.put(kernelRunner);
+
+      Holder holder2 = new Holder(data2);
+      holder2.put(kernelRunner);
+
+      Holder target = new Holder(new int[data1.length]);
+      target.put(kernelRunner);
+
+      HolderSumKernel kernel = new HolderSumKernel(holder1, holder2, target, data1.length);
+      kernelRunner.execute(kernel, data1.length);
+
+      target.get(kernelRunner);
+
+      for (int i = 0; i < data1.length; i++) {
+         int i1 = data1[i];
+         int i2 = data2[i];
+
+         assertThat(target.getData()[i], is(i1 + i2));
+      }
+
+      assertThat(kernelRunner.getExecutionMode(), is(EXECUTION_MODE.GPU));
+   }
+
+   @After
+   public void after() {
+      kernelRunner.dispose();
    }
 }
